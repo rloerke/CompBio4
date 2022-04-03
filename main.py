3c863de2384b88c03c305e72a78ee1606a451676
rloerke <rloerke@iwu.edu>
2022-04-03 03:39:01 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+Comp Bio Assignment 4
+Written by Ray Loerke
+BioGRID MITAB File Processor
+"""
+
+
+def main():
+    print("Hello World!")
+    get_physical_interaction_codes("mi.owl")
+
+
+def get_physical_interaction_codes(filename):
+    print("Phys Codes")
+    mio_fileread_helper(filename)
+
+
+def get_experimental_codes():
+    print("Exp Codes")
+
+
+def mio_fileread_helper(filename):
+    print("MIO helper")
+
+
+def process_biogrid_file():
+    print("Process BioGRID")
+
+
+main()
